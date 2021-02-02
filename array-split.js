03bea8dfb3d67c63c1407e952e277ec8cb867374
Saimun Hossain <saimunhossaiemon@gmail.com>
2021-02-02 18:42:46 +0600

    all work done

---
+++ array-split.js
@@ -0,0 +1,11 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const part = nums.slice(2, 5);
+
+const removed = nums.splice(2, 3, 77);
+// console.log(removed);
+// console.log(nums);
+
+
+
+const together = nums.join("saimun");
+console.log(together);
